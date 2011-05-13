3bdbe14ed2d3cd35685400b59d75ab5194d736aa
Sebastian Parborg <darkdefende@gmail.com>
2011-05-13 23:04:40 +0200

    Fixed so you can open files that is not pure uft8

--- scanfiles.py
+++ scanfiles.py
@@ -18,81 +18,86 @@ def scandir(dir, filetype):
 
 #lex stuff begins here
 
+def scanincludes(string):
+    tokens = (
+            "INCLUDE",
+            "GLOBH",
+            "LOCALH",
+            "BUNDLEINC"
+            )
+
+    t_ignore = " \t"
+
+    def t_comment(t):
+        r'(/\*(.|\n)*\*/)|(//.*)'
+        pass
+
+    def t_INCLUDE(t):
+        r"\#[Ii][Nn][Cc][Ll][Uu][Dd][Ee]"
+        return t
+
+    def t_GLOBH(t):
+        r"<.*\.h>"
+        t.value = t.value[1:-1] #strip <>
+        return t
+
+    def t_LOCALH(t):
+        r"\".*\.h\""
+        t.value = t.value[1:-1] #strip ""
+        return t
+
+    def t_BUNDLEINC(t): #for <string> etc.
+        r"<.*>"
+        return t
+
+    def t_error(t):
+        #print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+    lexer = lex.lex()
+
+    #lexer.input(input_string)
+    #
+    #for tok in lexer:
+    #    print(tok)
+    #
+    #YACC stuff here
+
+    def p_includes2(p):
+        """
+        includes : includes ginc
+                 | includes linc
+        """
+
+    def p_includes(p):
+        """
+        includes : ginc
+                 | linc
+        """
+
+    def p_ginclude(p):
+        "ginc : INCLUDE GLOBH"
+        global_hfiles.add(p[2])
+
+    def p_linclide(p):
+        "linc : INCLUDE LOCALH"
+        local_hfiles.add(p[2])
+
+    def p_error(p):
+        #print("syntax error at '%s'" % p.value)
+        pass
+
+    yacc.yacc()
+
+    yacc.parse(string)
+
 input_string = ""
 
-with open("test.h", encoding="utf-8") as inputfile:
-    input_string = inputfile.read()
-    print(input_string)
+for file in scandir("/home/zed/Desktop/test/smw/", ".h"):
+    print(file)
 
-tokens = (
-        "INCLUDE",
-        "GLOBH",
-        "LOCALH",
-        "BUNDLEINC"
-        )
+    with open(file, encoding="utf-8", errors="replace") as inputfile:
+        scanincludes(inputfile.read())
 
-t_ignore = " \t"
-
-def t_comment(t):
-    r'(/\*(.|\n)*\*/)|(//.*)'
-    pass
-
-def t_INCLUDE(t):
-    r"\#[Ii][Nn][Cc][Ll][Uu][Dd][Ee]"
-    return t
-
-def t_GLOBH(t):
-    r"<.*\.h>"
-    t.value = t.value[1:-1] #strip <>
-    return t
-
-def t_LOCALH(t):
-    r"\".*\.h\""
-    t.value = t.value[1:-1] #strip ""
-    return t
-
-def t_BUNDLEINC(t): #for <string> etc.
-    r"<.*>"
-    return t
-
-def t_error(t):
-    #print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
-
-lexer = lex.lex()
-
-#lexer.input(input_string)
-#
-#for tok in lexer:
-#    print(tok)
-#
-#YACC stuff here
-
-def p_includes2(p):
-    """
-    includes : includes ginc
-             | includes linc
-    """
-
-def p_includes(p):
-    """
-    includes : ginc
-             | linc
-    """
-
-def p_ginclude(p):
-    "ginc : INCLUDE GLOBH"
-    needed_hfiles.add(p[2])
-
-def p_linclide(p):
-    "linc : INCLUDE LOCALH"
-    needed_hfiles.add(p[2])
-
-def p_error(p):
-    #print("syntax error at '%s'" % p.value)
-    pass
-
-yacc.yacc()
-
-print(yacc.parse(input_string))
-print(needed_hfiles)
+print(global_hfiles)
+print(local_hfiles)
